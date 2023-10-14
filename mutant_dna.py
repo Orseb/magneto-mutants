eaeabd255edea02d37ed69c4c1b81053622daeb4
Seba Ortiz <sebious.ortizx2@gmail.com>
2023-10-14 16:14:35 -0300

    comentarios en mutant_dna

--- mutant_dna.py
+++ mutant_dna.py
@@ -3,6 +3,7 @@ def is_mutant(dna):
     # Funcion lambda que recorre una lista y comprueba si hay cuatro coincidencias entre los elementos
     check_pattern = lambda data: any(data[i:i+4].count(data[i]) == 4 for i in range(len(data) - 3))
     
+    # Variable que se va a encargar de contar los patrones que se encuentren
     found_patterns = 0
 
     # Verificamos los patrones en las filas
@@ -74,21 +75,28 @@ if __name__ == '__main__':
 
     dna = []
 
+    # Por cada una de las 6 filas
     for x in range(6):
+
         while True:
             
+            # Pedimos las 6 letras de la fila 
             row = list(input(f"Ingrese los 6 elementos de la fila {x+1}: ").upper())
 
+            # Hasta que no se cumpla la condicion de que tenga 6 letras y sean letras validas no salimos del bucle
             if len(row) == 6 and all(x in 'ATCG' for x in row):
                 dna.append(row)
                 break
             else:
                 print("Ingrese la cantidad correcta de elementos y/o letras permitidas")
     
+    # Mostramos el ADN
     print("Las bases nitrogenadas del ADN son:")
     for base in dna:
         print(base)
 
+    # Guardamos el resultado en una variable
     result = is_mutant(dna)
     
+    # Mostramos el resultado
     print(f"La persona es mutante? {result}")
