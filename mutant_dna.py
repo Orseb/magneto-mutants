80cd0790863060aad93bef9292f5767f23ff6f54
Seba Ortiz <sebious.ortizx2@gmail.com>
2023-10-19 18:25:29 -0300

    re-estructuracion del codigo

--- mutant_dna.py
+++ mutant_dna.py
@@ -1,75 +1,7 @@
-def is_mutant(dna):
-    
-    # Funcion lambda que recorre una lista y comprueba si hay cuatro coincidencias entre los elementos
-    check_pattern = lambda data: any(data[i:i+4].count(data[i]) == 4 for i in range(len(data) - 3))
-    
-    # Variable que se va a encargar de contar los patrones que se encuentren
-    found_patterns = 0
+import sys
 
-    # Verificamos los patrones en las filas
-    for row in dna:
-        if check_pattern(row):
-            found_patterns += 1
-
-    # Verificamos los patrones en las columnas
-    for j in range(6):
-        column = [dna[i][j] for i in range(6)]
-        if check_pattern(column):
-            found_patterns += 1
-
-    # Verificamos los patrones en las diagonales principales
-    main_diagonal1 = [dna[i][i] for i in range(6)]
-    main_diagonal2 = [dna[5-i][i] for i in range(5, -1, -1)]
-
-    if check_pattern(main_diagonal1):
-        found_patterns += 1
-
-    if check_pattern(main_diagonal2):
-        found_patterns += 1
-
-    # Verificamos los patrones en las diagonales secundarias izquierdas
-    secondary_diagonal1 = [dna[i][i-1] for i in range(1, 6)]
-    secondary_diagonal2 = [dna[i-1][i] for i in range(1, 6)]
-
-    if check_pattern(secondary_diagonal1):
-        found_patterns += 1
-
-    if check_pattern(secondary_diagonal2):
-        found_patterns += 1
-
-    # Verificamos los patrones en las diagonales secundarias derechas
-    secondary_diagonal3 = [dna[5-i][i-1] for i in range(5, 0, -1)]
-    secondary_diagonal4 = [dna[5-i][i+1] for i in range(4, -1, -1)]
-
-    if check_pattern(secondary_diagonal3):
-        found_patterns += 1
-
-    if check_pattern(secondary_diagonal4):
-        found_patterns += 1
-
-    # Verificamos los patrones en las diagonales terciarias izquierdas
-    tertiary_diagonal1 = [dna[i][i-2] for i in range(2, 6)]
-    tertiary_diagonal2 = [dna[i][i+2] for i in range(4)]
-
-    if check_pattern(tertiary_diagonal1):
-        found_patterns += 1
-
-    if check_pattern(tertiary_diagonal2):
-        found_patterns += 1
-
-    # Verificamos los patrones en las diagonales terciarias derechas
-    tertiary_diagonal3 = [dna[5-i][i-2] for i in range(5, 1, -1)]
-    tertiary_diagonal4 = [dna[6-i][i+1] for i in range(4, 0, -1)]
-
-    if check_pattern(tertiary_diagonal3):
-        found_patterns += 1
-
-    if check_pattern(tertiary_diagonal4):
-        found_patterns += 1
-
-    # Devolvemos la comprobacion
-    return found_patterns > 1
-
+sys.path.append("C:/Users/RYZEN/Desktop/Ejercicio ADN/magneto-mutants")
+from functions.mutant_check import is_mutant
 
 if __name__ == '__main__':
 
---
+++ tests/test_is_mutant.py
@@ -0,0 +1,98 @@
+import unittest
+import sys
+
+sys.path.append("C:/Users/RYZEN/Desktop/Ejercicio ADN/magneto-mutants")
+from functions.mutant_check import is_mutant
+
+class TestIsMutant(unittest.TestCase):
+
+    def test_rows(self):
+        dna = [
+            ['A', 'A', 'A', 'A', 'T', 'G'],
+            ['T', 'G', 'C', 'A', 'G', 'T'],
+            ['G', 'C', 'T', 'T', 'C', 'C'],
+            ['C', 'C', 'C', 'C', 'T', 'G'],
+            ['G', 'T', 'A', 'G', 'T', 'C'],
+            ['A', 'G', 'T', 'C', 'A', 'C']
+        ]
+        self.assertTrue(is_mutant(dna))
+
+    def test_columns(self):
+        dna = [
+            ['A', 'G', 'A', 'A', 'T', 'G'],
+            ['T', 'G', 'C', 'A', 'G', 'T'],
+            ['G', 'C', 'T', 'T', 'C', 'C'],
+            ['G', 'T', 'C', 'C', 'T', 'C'],
+            ['G', 'T', 'A', 'G', 'T', 'C'],
+            ['G', 'G', 'T', 'C', 'A', 'C']
+        ]
+        self.assertTrue(is_mutant(dna))
+
+    def test_main_diagonals(self):
+        dna = [
+            ['A', 'G', 'A', 'A', 'T', 'G'],
+            ['T', 'A', 'C', 'A', 'G', 'T'],
+            ['G', 'C', 'A', 'G', 'C', 'C'],
+            ['T', 'T', 'G', 'A', 'T', 'G'],
+            ['G', 'T', 'A', 'G', 'T', 'C'],
+            ['A', 'G', 'T', 'C', 'A', 'A']
+        ]
+        self.assertTrue(is_mutant(dna))
+
+    def test_secondary_left_diagonals(self):
+        dna = [
+            ['A', 'T', 'A', 'A', 'T', 'G'],
+            ['G', 'T', 'T', 'A', 'G', 'T'],
+            ['G', 'G', 'C', 'T', 'C', 'G'],
+            ['T', 'T', 'G', 'A', 'T', 'G'],
+            ['G', 'T', 'A', 'G', 'T', 'C'],
+            ['A', 'G', 'T', 'C', 'A', 'A']
+        ]
+        self.assertTrue(is_mutant(dna))
+
+    def test_secondary_right_diagonals(self):
+        dna = [
+            ['A', 'T', 'A', 'A', 'T', 'G'],
+            ['G', 'T', 'A', 'T', 'G', 'A'],
+            ['G', 'C', 'T', 'T', 'A', 'G'],
+            ['T', 'T', 'T', 'A', 'G', 'G'],
+            ['G', 'T', 'A', 'G', 'T', 'C'],
+            ['A', 'G', 'T', 'C', 'A', 'A']
+        ]
+        self.assertTrue(is_mutant(dna))
+
+    def test_tertiary_left_diagonals(self):
+        dna = [
+            ['A', 'T', 'G', 'A', 'T', 'G'],
+            ['G', 'T', 'A', 'G', 'T', 'A'],
+            ['C', 'C', 'T', 'T', 'G', 'G'],
+            ['T', 'C', 'T', 'A', 'G', 'G'],
+            ['G', 'G', 'C', 'G', 'T', 'C'],
+            ['A', 'G', 'T', 'C', 'A', 'A']
+        ]
+        self.assertTrue(is_mutant(dna))
+
+    def test_tertiary_right_diagonals(self):
+        dna = [
+            ['A', 'T', 'G', 'A', 'T', 'G'],
+            ['G', 'T', 'A', 'T', 'T', 'A'],
+            ['A', 'A', 'T', 'T', 'G', 'G'],
+            ['A', 'C', 'T', 'A', 'G', 'T'],
+            ['G', 'G', 'A', 'G', 'T', 'C'],
+            ['A', 'G', 'G', 'C', 'A', 'A']
+        ]
+        self.assertTrue(is_mutant(dna))
+
+    def test_non_mutant(self):
+        dna = [
+            ['A', 'T', 'G', 'A', 'T', 'G'],
+            ['G', 'T', 'C', 'T', 'T', 'A'],
+            ['A', 'A', 'T', 'T', 'G', 'G'],
+            ['A', 'C', 'T', 'A', 'G', 'T'],
+            ['G', 'G', 'A', 'T', 'T', 'C'],
+            ['A', 'G', 'G', 'C', 'A', 'A']
+        ]
+        self.assertFalse(is_mutant(dna))
+
+if __name__ == '__main__':
+    unittest.main()
